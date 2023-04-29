82de79fb06c10143bbc7b3303d0c13784dd8e98a
Koko-OwO <hcamargo03@misena.edu.co>
2023-04-29 14:52:10 -0500

    prueba de carousel automatico con bases de slider

--- js/main.js
+++ js/main.js
@@ -1,22 +1,31 @@
-window.addEventList.toggle('scroll', funtion()
-    .classList.toggle('active', window.scrollY >0)
-)
 
 
 
 
-const animationImages = document.querySelectorAll('.gato');
+// const animationImages = document.querySelectorAll('.gatoSubiendo');
 
-function animateIn() {
-  animationImages.forEach(image => {
-    const elementTop = image.getBoundingClientRect().top;
-    const windowHeight = window.innerHeight;
+// function animateIn() {
+//   animationImages.forEach(image => {
+//     const elementTop = image.getBoundingClientRect().top;
+//     const windowHeight = window.innerHeight;
 
-    if (elementTop < windowHeight) {
-      image.classList.add('animate-in');
-    }
-  });
-}
+//     if (elementTop < windowHeight) {
+//       image.classList.add('animate-in');
+//     }
+//   });
+// }
 
-window.addEventListener('scroll', animateIn);
-window.addEventListener('load', animateIn);
+// window.addEventListener('scroll', animateIn);
+// window.addEventListener('load', animateIn);
+
+
+// window.addEventListener("scroll", function(){
+//   var nav = document.querySelector("nav");
+//   nav.classList.toggle("top",this.window.scrollY>0)
+// })
+
+// window.addEventListener("scroll", function(){
+//   var nav = document.querySelector(".container");
+//   nav.classList.toggle("altu",this.window.scrollY>0)
+// })
+
